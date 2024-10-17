880ca7c24a2767a45bff4d07406a0e475d51ec8a
Laszlo Preznyak <laszlopreznyak11@gmail.com>
2024-10-17 16:19:29 +0200

    Decorating BeerOrderLine with beer data from beer-service.

---
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerDto.java
@@ -0,0 +1,21 @@
+package guru.sfg.beer.order.service.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private BigDecimal price;
+}
